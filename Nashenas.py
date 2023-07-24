f4c35c8d78c525dcf20c3f52d9bf4f436323881e
godcyberevil <140283734+godcyberevil@users.noreply.github.com>
2023-07-24 06:23:21 +0330

    Nashenas.py
    apt update
    apt upgrade
    pkg install git
    pkg install python
    pkg install python2
    pip install request 
    pip install mechanize
    pip install lolcat
    pip install bs4
    pkg install rich
    git clone https://github.com/godcyberevil/Nashenas.git
    ls
    cd nashenas
    ls
    python nashenas.py

---
+++ Nashenas.py
@@ -0,0 +1,296 @@
+import os
+import os,sys,time
+import time                                                     
+import sys                                                      
+import colored                                                  
+import colorama                                                
+import datetime
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style
+print (f"{Fore.RED} INSTAALL CYBER DEVIL")
+os.system("pip install cyber devil")
+os.system("clear")
+print(f'{Fore.YELLOW} INSTALL cyber evil')
+os.system('pip install terrifier')
+os.system('clear')
+print(f'{Fore.GREEN} INSTALL TERRIFIER')
+os.system('pip install terrifier.cyber evil ')
+green = "\033[32m"
+red = "\033[31m"
+blue = "\033[36m"
+pink = "\033[35m"
+yellow = "\033[93m"
+darkblue = "\033[34m"
+white = "\033[00m"
+mark = '\x1b[38;5;4m'
+mark1 = '\x1b[48;5;15m'
+mark2 = '\x1b[0m'
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+print(f"{Fore.YELLOW} ")
+os.system('clear')
+while True:
+        Mjie = input (" Please enter the password 🔐> ")
+        if Mjie != "cyber-devil":
+                print(Fore.RED+'PASSWORD incorrect')
+                continue
+        else:
+                break
+os.system("clear")
+
+print (Fore.BLUE + "")
+
+x = str(datetime.datetime.now())
+
+print("started " + (x))
+
+print(f"{red} ")
+x = (Fore.CYAN+f"""                   THE SCRIPT FILTERING                                   GOD CYBER   TERRIFIER   CYBER EVIL   LUCIFER 
+                          _cyqyc_
+                      :>3qKKKKKKKq3>:
+                  ';CpKKKKKKKKKKKKKKKpC;'
+              -"iPKKKKKKKKKKKKKKKKKKKKKKKPi"-
+          ~v]KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK]v~
+       ,rwKKKKKKKKKKKKKPv;,:'-':,;vPKKKKKKKKKKKKKwr,
+      !KKKKKKKKKKKKKKK/             !KKKKKKKKKKKKKKK!
+      !KKKKKKKKKKKKKKf               CKKKKKKKKKKKKKK!
+      !KKKKKKKKKKKKKp-               -qKKKKKKKKKKKKK!
+      !KKKKKKKKKKKKK>"               "\KKKKKKKKKKKKK!
+      !KKKKKKKw;,_'-                   .-:,"wKKKKKKK!
+      !KKKKKKKKhi*;"                   ";*ihKKKKKKKK!
+      !KKKKKKKKKKKKK;                 ;KKKKKKKKKKKKK!
+      !KKKKKKKKKKKKK2>'             '>2KKKKKKKKKKKKK!
+      !KKKKKKKKKKKKKKKZ             ZKKKKKKKKKKKKKKK!
+      !KKKKKKKKKKKKKKK5             eKKKKKKKKKKKKKKK!
+      !KKKKKKKKKKKqC;-               -;CqKKKKKKKKKKK!
+      <KKKKKKKKkr,                       ,rSKKKKKKKK<
+       -"v]qj;-                             -;jq]v"-
+               
+""")
+for c in x:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.02)
+#⁶6⁶
+print(f"{red} ")
+x = ("""
+   im creator TerriFieR
+   im creator cyber
+   im cyber devil
+   im cyber evil
+   im cyber lucifer
+   im cyber eblis
+   im TerriFieR NaShEnAs
+   im leader bloody404
+   
+  
+  """)
+for x in c:
+        sys.stdout.write(x)
+        sys.stdout.flush()
+        time.sleep(0.02)
+# ostoreh faramosh shode *_*
+time.sleep(4.3)
+print(f"{pink} ")
+print ("  ok now go to ids ⇾")
+print (Fore.YELLOW + "")
+time.sleep(1.7)
+print ("               ID SHAD @cyber_evil")
+print ("               ID RUBIKA @savaking")
+print ("               ID TELEGRAM @maker_cyber_devil - @maker_cyber_evil")
+print(f"{Fore.GREEN} ")
+time.sleep(1.0)
+print (" /<>/")
+time.sleep(0.6)
+print (" /<<>>/")
+time.sleep(0.5)
+print (" /<<<>>>/")
+time.sleep(0.4)
+print (" /<<<<>>>>/")
+time.sleep(0.3)
+print (" /<<<DeViL>>>/")
+time.sleep(0.2)
+print (" /<<<<<<>>>>>>/")
+time.sleep(0.1)
+print (" /<<<<<<<>>>>>>>/")
+time.sleep(0.5)
+print (" /<<<<<<<<>>>>>>>>/")
+time.sleep(0.1)
+print (" /<<<<<<<<<>>>>>>>>>/")
+time.sleep(0.5)
+print (" /<<<<<TeRRiFieR>>>>>/")
+print ("")
+
+x = (Fore.BLUE+f'''
+                  .----.
+      .---------. | == |
+      |.-"""""-.| |----|
+      ||       || | == |
+      ||       || |----|
+      |'-.....-'| |::::|
+      "")---("" |___.|
+     /:::::::::::\" _  "
+    /:::=======:::\\\
+jps"""""""""""""  '-'
+''')
+for c in x:
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.02)
+# vared kardane addad baraye gereftan kod
+up = input (Fore.CYAN+f""" ---------------------------------------------------------
+  1 > code acc rub             11 > server          
+  2 > code acc shad            12 > video porn hub  
+  3 > link sex                 13 > lag             
+  4 > bug                      14 > code fil gap    
+  5 > dxript                   15 > code fil channel
+  6 > ip hasas                 16 > bug with tor    
+  7 > tarjome hasah            17 > logo            
+  8 > mokharab                 18 > algoritm        
+  9 > link daeshi              19 > site sepah      
+  10 > viros code              20 > mokharab sangin    
+                [>Enter The Number<]                    
+ ---------------------------------------------------------
+    | IM CREATOR CYBER or  CREATOR TERRIFIER  |
+    -----------------------------------------------
+          | $ put the number  >>>>>>""")
+for c in x:
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.0002)
+
+
+if up == "1":
+ print (' code kamel acc rubika ≠>' + '%s%s (https://ter.af-ter.af.ter(https://s4.uupload.ir/files/i1.40.xnxx.com.30.905)https://github.com/termux/termux-appnstallatio/wiki.termux.com/w((6.0.3.6/d///f//i///y.6.0.3.2.3.8.6.4.5.8.6.3.7.7.9.0.5.5.1.2.4.0.3.4))/Backin http://github.com/termux/termux-app/releashttps://github.com/termuterm.BS.1111.CD.Filtering-Rubika.1/1/1/1.yff01k)(xnxx.com)(/6.9.3.8/f//d/1.4.8.2.8.9.5.1.3.2.1.1.4.4.4.6/)robika.https://s6.uupload.ir/files/_o6s4.jpeg.fyll.www.xxnx) %s' % (fg('red'), bg('yellow'), attr('reset')))
+ 
+
+
+if up == "2":
+         print (' code kamel acc shad ≠>' + '%s%s (ravesh fil acc shad -> aval mitrid pv   fard va fosh midid vaghti onam dad shoma fosh khodeton ro pack   mikonid va ye screen shat migirid va be id fata shad id fata    ➣@FataReport va bad az dadan gozaresh mirid code >https://s2.uupload.ir/files/1_rdq.png.https://tinyurl.com/uppdatesnew.of.sexchat.with.users.causng.disturbance.to.others.will.be/filtered.Filter.Shad.in_/(http://dxprit-dayson-payambar-noob-hack-infect.phpnet.us/dxprit.html< ro 20 bar dayer va 50 mostahjan      mizanid va acc podr mishe) %s' % (fg('red'), bg('yellow'), attr('reset')))
+
+
+if up == "3":
+        print (' link sex ≠>' + '%s%s (https://s2.uupload.ir/files/1_rdq.png  ~  https://s6.uupload.ir/files/download_(9)_ie3p.jpg  ~  https://s6.uupload.ir/files/download_(4)_49pt.jpg  ~  https://imgurl.ir/uploads/u497134_VID_20230401_204306_495.mp4) %s' % (fg('red'), bg('yellow'), attr('reset')))
+
+
+
+
+if up == "4":
+
+        print (' bug ≠>' + '%s%s (/http://1.4.2//f///y//d.3.5.2.4.1.4.5.4.5.5.6.5.5.5.8.5.5.2/) %s' % (fg('red'), bg('yellow'), attr('reset')))
+
+
+
+if up == "5":
+        print (' dxript ≠>' + '%s%s (http://dxprit-dayson-payambar-noob-hack-infect.phpnet.us/dxprit.html  ~  http://dxprit-hack-yftt15k-filter-supportbot-fil.phpnet.us/x.photo.html  ~  http://dxprit-supportbot-hacker-bakhtiari-fil.phpnet.us/fil.rubika.html  ~  http://dxprit-filtering-rubika-174.gigfa.com/dxprit.html  ~  https://jizzbunker-com.translate.goog/?_x_tr_sl=en&_x_tr_tl=fa&_x_tr_hl=fa&_x_tr_pto=sc) %s' % (fg('red'),bg('yellow'), attr('reset')))
+
+if up == "6":
+	print (' ip haye hasas ≠>' + '%s%s (IP rubika ≠> 5.106.8.151  ~  IP dark web ≈> 108.138.106.118  ~  IP web ananimous ≈> 204.8.99.146  ~  IP xnxx ≈> 185.88.181.60  ~  IP xxxx ≈> 54.153.216.130  ~  IP pornhub ≈> 192.99.158.243) %s' % (fg('red'),bg('yellow'), attr('reset')))
+
+if up == "7":
+	print (' tarjomeh hasas en ≠>' + '%s%s (He uses obscenity in this account and this account forces people to commit sins and makes people anti-Islamic revolution. This account hacks and filters people and scares people to force imams, prophets and the supreme leader of the revolution. He curses Islam and teaches people to be anti-revolutionary and scares people with Momos photo and threatens children with this photo to be anti-revolutionary and broadcasts pornographic films and (S.C.) photos. Please solve it) %s' % (fg('red'),bg('yellow'),attr('reset')))
+	
+if up == "8":
+	print (' mokharab≈>' + '%s%s (https://cigarettes-chemical-rid-numeric.trycloudflare.com  ~  https://bit.li/3ild93L/php-hack  ~  https://bit.li/3ild93L  ~  https://raw.githubusercontent.com/Spam404/lists/master/main-blacklist.txt  ~  https://bit.ly/3fX8ljZ/fil.rubika  ~  http://ffmodmenu.page.link/Anti-V  ~  http://vxvault.net/URL_List.php  ~  https://v.3.0.9.yftt18k.in.xxx.com  ~  http://fil.sex.com/Distribution.immoral.content.com  ~  http://naturerleben-xhain.berlin/kieznaturkarte  ~  https://book.hacktricks.xyz  ~  http://zqktlwi4fecvo6ri.onion/wiki/index.php  ~  https://crash-bandicoot.info/enfejar/gambling-site  ~  https://dl.uploadgram.me/60bdffd3a2be4h  ~  https://cigarettes-chemical-rid-numeric.trycloudflare.com) %s' % (fg('red'),bg('yellow'),attr('reset')))
+	
+if up == "9":
+	print(' link daeshi≠>' + '%s%s (https://s6.uupload.ir/files/750242_178_lelj.jpg) %s' % (fg('red'),bg('yellow'),attr('reset')))
+	
+if up == "10":
+	print(' virus code≠>' + '%s%s (http://txt//1-2-3-4.V-iruss.kabir/sheytan.bog.text.Account.rubika  ~  http://ffmodmenu.page.link/Anti-V  ~  http://54flq67kqr5wvjqf.onion/python ) %s' % (fg('red'),bg('yellow'),attr('reset')))
+	
+if up == "11":
+	print('server≠>' + '%s%s (v.3.1.1.yftt15k.in  ~  /yftt16k  ~  /yftt15k  ~  /yftt16k  ~  /yftt15k  ~  /yftt16k  ~  /yftt15k  ~  /yftt_08  ~  /yftt_18  ~  /yftt_08  ~  /yftt_18  ~  /yftt_08  ~  /yftt_18  ~  /yttk  ~  filter.rubika  ~  gif.sxs.com  ~  /yttk-18963  ~  yttk-26357) %s' % (fg('red'),bg('yellow'),attr('reset')))
+
+if up == "12":
+	print('video porn hub≠>' + '%s%s (https://www.pornhub.com/view_video.php?viewkey=ph62541e245952b  ~  https://www.pornhub.com/view_video.php?viewkey=ph62541e492c3a7  ~  https://www.pornhub.com/interstitial?viewkey=ph6203e4bf1b1d9) %s' % (fg('red'),bg('yellow'),attr('reset')))
+
+if up == "13":
+	print('lag≠>' + '%s%s (https://jizzbunker-com.translate.goog/?_x_tr_sl=en&_x_tr_tl=fa&_x_tr_hl=fa&_x_tr_pto=sc  ~  http://xa.fltvuq.cn/?account.Code=10749999999172-Shervi.Gazme.zan-hackers65.78766.infect.in.29*offs.ec/2Mcezwr) %s' % (fg('red'),bg('yellow'),attr('reset')))
+
+if up == "14":
+	print('code filter gap≠>' + '%s%s (https://4.0.0.1.1.5.3.8.0.14.7.3./f/g/c/c/c/1.7.8.2.6.0.0.3.4.1.////1.2.6.5.8.4.0.4.6.10-4)(v.3.0.9.yftt15XXX.in)(codetor)(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2jEofU_oZSgmuTJ_jjCwp5fX0OMNHnzak2Q&usqp=CAU)(linkgap)(0.4.6.10-4)(codetor)(http://dxprit.yftt-filter-rubika.hackers923023.dscript.com)(v.3.0.9.xxx)(http://4.1.6.0.4.1.4.5.5.6.4.0.0.2.9.6.7.4.6.3.4///1.5.6.7.8.2.1.6     code tor va link gap ro jaee ke neveshtam bezarid va code amade misheh ) %s' % (fg('red'),bg('yellow'),attr('reset')))
+	
+if up == "15":
+	print('code filter channel≠>' + '%s%s (https://dxprit-yftt15k-sexy-in-rubika-hacker.github.io/hacker.github.io)(http://uupload.ir/files/img_20210728_194736_412_9t6h.jpgSxs.gif.sxs.txt.gif.sxs.com)(11.15/17.11.439)(http://ffmodmenu.page.link/Anti-V)(https://cigarettes-chemical-rid-numeric.trycloudflare.com.BS.1111.CD.Filtering-Rubika.1/1/1/1.yff01k)(https://IP.5.106.8.151.Rubika.Com.SupportBot) %s' % (fg('red'),bg('yellow'),attr('reset')))
+
+if up == "16":
+	print(' bag with code tor≠>' + '%s%s (https://xexx.filtring.rubika.af.af.af.af/02/03/37/0/0/0/02/03/37/0/0/0/02/03/37/0/0/0/02/03/37/0/0/0/) %s' % (fg('red'),bg('yellow'),attr('reset')))
+	
+if up == "17":
+	print(' logo≠>' + '%s%s (http://xa.fltvuq.cn/?account.Code=10749999999172-Shervi.Gazme.zan-hackers65.78766.infect.in.29*offs.ec/2Mcezwr) %s' % (fg('red'),bg('yellow'),attr('reset')))
+
+if up == "18":
+	print ('code ghavi ba code script≠>' '%s%s (http://f///h//c//a///4.2.5.7.4.3.4.6.7.9.8.5.2.5.5.2.5.7.9.5.2.3.6.8.0.0.9.6.1  ~  https://photo.sex.com/5.3.8.4/g/y///v//w/s//5.2.4.7.2.8.6.2.7.9.3.7.1.5.7.3.7.8.1.6.3.6.2.8.2com) %s' % (fg('red'),bg('yellow'),attr('reset')))
+	
+if up == "19":
+	print ('site sepah≠>' '%s%s (http://www.sepahnews.com.hack.porn.filtering.rubika) %s' % (fg('red'),bg('yellow'),attr('reset')))
+	
+if up == "20":
+	print ('mokharab sangin≠>' '%s%s (.fa*.af*.fa*(tor)(yftt15k)(v.3.0.9.yftt15k.in)filter.servr.rubika.com).af.*fa.*af.*fa.*af.*fa.*af.*fa.*.com   faghat code tor bezarid) %s' % (fg('red'),bg('yellow'),attr('reset')))
+time.sleep(0.6)
+print(f"{red}" )
+x = """
+                        .xUHWH!! !!?Cybering:.
+                      .X*#M@$!!  !X!M$$$$$$WWx:.
+                    :!!!!!!?H! :!$!$$$$$$$$$$8X:
+                    !!~  ~:~!! :~!$!#$$$$$$$$$$8X:
+                    :!~::!H!<   ~.U$X!?R$$$$$$$$MM!
+                    ~!~!!!!~~ .:XW$$$U!!?$$$$$$RMM!
+                    !:~~~ .:!M"T#$$$$WX??#MRMMDM!
+                      ~?WuxiW*   "#$$$$8!!!!??!!!
+                    :X- M$$$$       "T#$T~!8$WUXU~
+                    :%  ~#$$$m:        ~!~ ?$$$$$$
+                :!.-   ~T$$$$8xx.  .xWW- ~""##*"
+        .....   -~~:< !    ~?T#$$@@W@*?$$      /
+        W$@@M!!! .!~~ !!     .:XUW$W!~ "~:    :
+        #"~~.:x%!!  !H:   !WM$$$$Ti.: .!WUn+!
+        :::~:!!:X~ .: ?H.!u "$$$B$$$!W:U!T$$M~
+        .~~   :X@!.-~   ?@WTWo("*$$$W$TH$!
+        Wi.~!X$?!-~    : ?$$$B$Wu("**$RM!
+        $R@i.~~ !     :   ~$$$$$B$$en:`
+        ?MXT@Wx.~    :     ~"##*$$$$$$
+     """
+                          
+for c in x:
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.02)
+	
+print (Fore.WHITE + " ")
+time.sleep(1.8)
+print (f"""
+{green}
+ made by 2 ostoreh faramosh shode
+ 
+ user tel khalegh cyber @maker_cyber_devil - @maker_cyber_evil
+ 
+ user shad khalegh cyber @cyber_evil
+ 
+ user rubika khalegh cyber @savaking
+ """)
+time.sleep(1.8)
+x = (f""" {Blue}
+
+       .!!!!!:.                        .:!!!!!!!!!!!!
+       ~~~~!!!!!!.                 .:!!!!!!!!!UWWW$$$
+      :$$NWX!!:           .:!!!!!!XUWW$$$$$$$$$P
+      $$$$$##WX!:      .<!!!!UW$$$$"  $$$$$$$$#
+      $$$$$  $$$UX   :!!UW$$$$$$$$$   4$$$$$*
+      ^$$$B  $$$$\     $$$$$$$$$$$$   d$$R"
+        "*$bd$$$$      '*$$$$$$$$$$$o+#"
+
+""")
+for c in x:
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.02)
+time.sleep(1)
+print(" ")
+print(" ")
+time.sleep(2.0)
+print (f"{red} this is our end of our work - im DeViL - TeRRiFieR")
+time.sleep(9999999.0
